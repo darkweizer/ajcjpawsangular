2e4069aaa11175f9aaa1688408e016b8a0193f95
darkweizer <darkweizer@gmail.com>
2018-11-21 15:30:42 +0100

    add type meal entity

--- src/main/java/com/formation/ajcjpaawangular/jpa/ProductsEntity.java
+++ src/main/java/com/formation/ajcjpaawangular/jpa/ProductsEntity.java
@@ -25,7 +25,31 @@ public class ProductsEntity {
     @OneToMany(mappedBy = "product")
     private List<OrderRowEntity> ordersRowList;
 
+    @ManyToMany(
+            cascade= { CascadeType.PERSIST, CascadeType.MERGE}
+
+    )
+    @JoinTable(
+            joinColumns=@JoinColumn(name="productsId"),
+            inverseJoinColumns=@JoinColumn(name="typeMealId")
+    )
+    private List<TypeMealEntity> typeMealList;
+
     @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
     @JoinTable(name = "products_familymealID")
     private FamilyMealEntity familyMealEntity;
+
+    public ProductsEntity() {
+    }
+
+    public ProductsEntity(String img, Double price, String dispo, Integer stock, List<CommentEntity> commentList, List<OrderRowEntity> ordersRowList, List<TypeMealEntity> typeMealList, FamilyMealEntity familyMealEntity) {
+        this.img = img;
+        this.price = price;
+        this.dispo = dispo;
+        this.stock = stock;
+        this.commentList = commentList;
+        this.ordersRowList = ordersRowList;
+        this.typeMealList = typeMealList;
+        this.familyMealEntity = familyMealEntity;
+    }
 }
