a2173a0193d61ee6964c8c0ae0bc28f8c3a4e582
saoussane <saoussane@192.168.1.46>
2018-11-21 11:08:56 +0100

    revert

--- src/main/java/com/formation/App.java
+++ src/main/java/com/formation/App.java
@@ -1,8 +1,18 @@
 package com.formation;
 
-import com.formation.ajcjpaawangular.utils.HibernateUtils;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import com.formation.ajcjpaawangular.jpa.Adress;
+import com.formation.ajcjpaawangular.jpa.Comment;
+import com.formation.ajcjpaawangular.jpa.OrderHeader;
+import com.formation.ajcjpaawangular.jpa.OrderRow;
+import com.formation.ajcjpaawangular.jpa.User;
+import com.formation.ajcjpaawangular.utils.HibernateUtils;
 
 public class App {
 
@@ -22,10 +32,58 @@ public class App {
         testCreate();
     }
 
-    private static void testCreate() {
+ private static void testCreate() {
+    	
+    	// Persistante objects created
+	 
+       	// add user
+    	User user = new User( "lastName", "firstName", new Date(), "password", "genre", "email","society", false);
+    	OrderHeader order = new OrderHeader ("type",new Date(), 20.5);
+    	OrderHeader order2 = new OrderHeader ("type2",new Date(), 10.5);
+    	Comment comment = new Comment ("libelle", "body");
+    	Comment comment2 = new Comment ("libelle2", "body2");
+    	OrderRow orderRow = new OrderRow(2, 5.55);
+    	Adress adress = new Adress("libelle Adress", "street", 2500); 
+    	Adress adress2 = new Adress("libelle Adress2", "street2", 2600); 
+    	
+    	List<Adress> adressList= new ArrayList<Adress>();
+    	adressList.add(adress);
+    	adressList.add(adress2);
+    	
+    	adress.setUser(user);
+    	adress2.setUser(user);
+    	user.setAdressList(adressList);
+    	
+    	List<Comment> commentList =    new ArrayList<Comment>();
+    	commentList.add(comment);
+    	commentList.add(comment2);
+    	
+    	comment.setUser(user);
+    	comment2.setUser(user);
+    	user.setCommentList(commentList);
 
-        // Persistante objects created
+    	List<OrderHeader> listOrder= new ArrayList<OrderHeader>();
+    	listOrder.add(order);
+    	listOrder.add(order2);
+    	
+    	order.setUser(user);
+    	order2.setUser(user);
+    	user.setOrderList(listOrder);
+    	
+    	s.persist(adress);
+    	s.persist(adress2);
+    	s.persist(comment);
+    	s.persist(comment2);
+    	s.persist(order);
+    	s.persist(order2);
+    	s.persist(user);
+    	s.persist(orderRow);
+    	
+    	
+    	// Save data
+    	
+    	tx.commit();
 
     }
-
+   
 }
