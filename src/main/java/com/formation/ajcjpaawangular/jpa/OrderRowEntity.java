6d7998ad90eafb5f3a9f9e0a250e12e5061dbcb6
darkweizer <darkweizer@gmail.com>
2018-11-21 16:13:19 +0100

    fix error remove code

--- src/main/java/com/formation/ajcjpaawangular/jpa/OrderRowEntity.java
+++ src/main/java/com/formation/ajcjpaawangular/jpa/OrderRowEntity.java
@@ -1,14 +1,6 @@
 package com.formation.ajcjpaawangular.jpa;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Data;
 
@@ -23,6 +15,11 @@ public class OrderRowEntity {
     private Integer id;
     private Integer quantie;
     private Double total;
+
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+    @JoinColumn(name = "products_orderrowID")
+    private ProductsEntity product;
+
 	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
 	@JoinColumn(name = "order_orderRow")
 	private OrdersEntity order;
